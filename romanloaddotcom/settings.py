27b701dc320a3d060bd9a951010132c0226393b9
zhengwenjie <zhengwenjie@digichain.cn>
2017-10-10 22:58:24 +0800

    use sqlite3 for local database

--- romanloaddotcom/settings.py
+++ romanloaddotcom/settings.py
@@ -111,8 +111,6 @@ WSGI_APPLICATION = 'romanloaddotcom.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'romanload',
         'USER': 'root',
@@ -121,6 +119,12 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+if DEBUG:
+    DATABASES.update(default={
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    })
 
 
 # Password validation
